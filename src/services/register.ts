0e792c2d52775842fadbe987e692058a147f2e19
Robson H. Rodrigues <robhenrod@gmail.com>
2023-12-04 15:55:37 -0300

    feat: basic user entity implementation

---
+++ src/services/register.ts
@@ -0,0 +1,42 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterServiceRequest {
+  name: string
+  email: string
+  password: string
+}
+
+interface RegisterServiceResponse {
+  user: User
+}
+
+export class RegisterService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterServiceRequest): Promise<RegisterServiceResponse> {
+    const password_hash = await hash(password, 6)
+
+    const emailAlreadyInUse = await this.usersRepository.findByEmail(email)
+
+    if (emailAlreadyInUse) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return {
+      user,
+    }
+  }
+}
