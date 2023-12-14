8f21fca8aee38c90208eb15659a52bedfee087ce
Robson Henrique Rodrigues <robhenrod@gmail.com>
2023-12-14 11:13:10 -0300

    feat: user routes
    - Authentication;
    - Refresh Token;
    - Profile;

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
 import { env } from '@/env'
+import fastifyCookie from '@fastify/cookie'
+import fastifyJwt from '@fastify/jwt'
 import fastify from 'fastify'
 import { ZodError } from 'zod'
 import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(usersRoutes)
 
